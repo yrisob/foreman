60671093b2963378248019eae37563843cc4fd2f
Yura Sobolev <yrisob@MacBook-Pro-Yura-2.local>
2019-08-29 12:43:40 +0300

    form contructor with opportunity to create a login form

---
+++ foreman-client/src/components/FormComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row v-for="(fieldItem, index) in fieldsList" :key="index">
+        <v-col cols="12" class="col-md-10 offset-md-1">
+          <v-text-field
+            v-if="fieldItem.type === 'text' || fieldItem.type === 'email'"
+            v-model="fieldItem.value"
+            :rules="fieldItem.rule"
+            :counter="fieldItem.counter ? fieldItem.counter : 0"
+            :label="fieldItem.title"
+            type="text"
+          ></v-text-field>
+          <v-text-field
+            v-if="fieldItem.type === 'password'"
+            v-model="fieldItem.value"
+            :rules="fieldItem.rule"
+            :counter="fieldItem.counter ? fieldItem.counter : 0"
+            :label="fieldItem.title"
+            :type="show ? 'text' : 'password'"
+            :append-icon="show ? 'visibility' : 'visibility_off'"
+            @click:append="show = !show"
+          ></v-text-field>
+        </v-col>
+        <v-col class="col-md-10 offset-md-1" v-if="fieldItem.linkedLink">
+          <a
+            class="primary--text caption uppercase"
+            @click="fieldItem.linkedLink.exec"
+          >
+            {{ fieldItem.linkedLink.title }}
+          </a>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <template v-for="(button, index) in buttonsList">
+            <div
+              :key="index + 'divPa'"
+              class="pa-1"
+              v-show="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm"
+            ></div>
+            <v-btn
+              :key="index + 'btn'"
+              :class="button.class"
+              outlined
+              @click="button.exec"
+            >
+              {{ button.name }}
+            </v-btn>
+          </template>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: ['fieldsList', 'buttonsList'],
+  data: () => ({
+    show: false
+  })
+}
+</script>
--- foreman-client/src/components/LoginForm.vue
+++ foreman-client/src/components/LoginForm.vue
@@ -14,14 +14,18 @@
           </v-list-item-content>
         </v-list-item-subtitle>
         <v-divider></v-divider>
-        <v-form>
+        <form-component
+          :fieldsList="loginFields"
+          :buttonsList="loginButtons"
+        ></form-component>
+        <!-- <v-form>
           <v-container>
             <v-row>
               <v-col cols="12" class="col-md-10 offset-md-1">
                 <v-text-field
                   v-model="email"
                   :rules="emailRules"
-                  :counter="10"
+                  :counter="0"
                   label="email"
                   required
                 ></v-text-field>
@@ -49,8 +53,7 @@
               <v-col cols="12">
                 <div class="pa-1"></div>
                 <v-btn
-                  class="col-md-2 col-xs-10  offset-md-1 offset-xs-1 caption"
-                  outlined
+                  class="col-md-2 col-xs-10  offset-md-1 offset-xs-1 caption v-btn--outlined"
                   >Войти</v-btn
                 >
                 <div
@@ -58,22 +61,60 @@
                   v-show="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm"
                 />
                 <v-btn
-                  class="col-md-4 offset-md-4  col-xs-10 offset-xs-1 caption"
-                  outlined
+                  class="col-md-4 offset-md-4  col-xs-10 offset-xs-1 caption v-btn--outlined"
                   >Зарегистрироваться</v-btn
                 >
               </v-col>
             </v-row>
           </v-container>
-        </v-form>
+        </v-form> -->
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import FormComponent from './FormComponent'
+
 export default {
+  components: {
+    FormComponent
+  },
   data: () => ({
+    loginFields: [{
+      type: 'email',
+      value: '',
+      title: 'email',
+      counter: 0,
+      rule: [
+        v => !!v || 'Требуется ввод email',
+        v => /.+@.+/.test(v) || 'Email должен соответсвенновть почтовым адресам'
+      ]
+    },
+    {
+      type: 'password',
+      value: '',
+      title: 'Пароль',
+      counter: 0,
+      rule: [
+        v => !!v || 'Требуется пароль',
+        v => v.length > 6 || 'Пароль должен быть более 6 символов'
+      ],
+      linkedLink: {
+        title: 'восстановить пароль',
+        exec: () => { alert('восстановить пароль') }
+      }
+    }],
+    loginButtons: [{
+      name: 'Войти',
+      class: 'col-md-2 col-xs-10  offset-md-1 offset-xs-1 caption v-btn--outlined',
+      exec: () => { alert('Войти') }
+    },
+    {
+      name: 'Зарегистироваться',
+      class: 'col-md-4 offset-md-4  col-xs-10 offset-xs-1 caption v-btn--outlined',
+      exec: () => { alert('Зарегистироваться') }
+    }],
     email: '',
     emailRules: [
       v => !!v || 'Требуется ввод email',
